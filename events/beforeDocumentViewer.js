2236dcc45b964238ad671ce914de046ebe0af863
Marcos Jahn <marcos.jahn@fluig.com>
2016-03-21 12:29:35 -0300

    Initial Commit

---
+++ events/beforeDocumentViewer.js
@@ -0,0 +1,24 @@
+function beforeDocumentViewer(){ 
+
+	//Informações do documento
+	var doc = getValue("WKDocument");
+	var docExpires = doc.getExpires();
+	
+	//Variáveis para manipualar datas
+	var sdfDate = new java.text.SimpleDateFormat("yyyy-MM-dd");
+	var now = new Date();
+	
+	//Formatando datas
+	var strDate = sdfDate.format(now);
+    var documentExpirationDate = sdfDate.format(doc.getExpirationDate());
+	
+    //Verificando se a data de validade do documento é igual a data atual
+    var documentExpiresToday = strDate.equals(documentExpirationDate)
+    
+	if(docExpires && documentExpiresToday){
+		log.warn("The document " + doc.getDocumentId() + " it cannot be viewed by user " + getValue("WKUser") + " because the due date is equal to the current date");
+		throw ("Esse documento está vencendo hoje, não pode mais ser visualizado.")
+	}else{
+		log.info("This documment is allowed to visualization.")
+	}
+}
