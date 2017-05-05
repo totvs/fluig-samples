ed89c7c4ef5ea2d8afb9a55b949bbe5962958a9a
Marcelo Leandro Pereira <marcelo.pereira@totvs.com.br>
2017-05-05 09:02:42 -0300

    Atualização de documentação

--- forms/smart-zoom/custom.js
+++ forms/smart-zoom/custom.js
@@ -3,64 +3,23 @@ function setSelectedZoomItem(selectedItem) {
 	var NAME = "colleagueName";
 	var EMAIL = "colleagueMail";
 	var ID = "colleagueId";
-	var itemSelected;	
+
 	var FIELD = selectedItem.inputId;
 	
 	if(selectedItem.inputId == NAME){		
-		itemSelected = {
-			     'colleagueId': selectedItem["colleagueId"],
-			     'colleagueName': selectedItem["colleagueName"],
-			     'login': selectedItem["login"],
-			     'mail': selectedItem["mail"]
-			};
+		setZoomData("colleagueMail", selectedItem["Email"]);
+		setZoomData("colleagueId", selectedItem["ID"]);
 		
-		setZoomData(filter_colleagueMail, itemSelected);
-		setZoomData(filter_colleagueId, itemSelected);
-		
-	}else if(selectedItem.inputId == EMAIL){
-		
-		itemSelected = {
-			     'colleagueId': selectedItem["colleagueId"],
-			     'colleagueName': selectedItem["colleagueName"],
-			     'login': selectedItem["login"],
-			     'mail': selectedItem["mail"]
-			};
-		
-		setZoomData(filter_colleagueName, itemSelected);
-		setZoomData(filter_colleagueId, itemSelected);		
+	}else if(selectedItem.inputId == EMAIL){			
+		setZoomData("colleagueName", selectedItem["Nome"]);
+		setZoomData("colleagueId", selectedItem["ID"]);		
 	
 	}else if(selectedItem.inputId == ID){
-		
-		itemSelected = {
-			     'colleagueId': selectedItem["colleagueId"],
-			     'colleagueName': selectedItem["colleagueName"],
-			     'login': selectedItem["login"],
-			     'mail': selectedItem["mail"]
-			};
-		
-		setZoomData(filter_colleagueName, itemSelected);
-		setZoomData(filter_colleagueMail, itemSelected);				
+		setZoomData("colleagueName", selectedItem["Nome"]);
+		setZoomData("colleagueMail", selectedItem["Email"]);				
 	}
-	removeZoomData();
 }
 
-function setZoomData(inputName, item){
-	inputName.add(item);
-}
-
-function removeZoomData(){
-	filter_colleagueName.on('fluig.autocomplete.itemRemoved', function(ev) {
-		filter_colleagueId.removeAll();
-		filter_colleagueMail.removeAll();
-	});
-	
-	filter_colleagueId.on('fluig.autocomplete.itemRemoved', function(ev) {
-		filter_colleagueName.removeAll();
-		filter_colleagueMail.removeAll();
-	});
-	
-	filter_colleagueMail.on('fluig.autocomplete.itemRemoved', function(ev) {
-		filter_colleagueName.removeAll();
-		filter_colleagueId.removeAll();		
-	});
+function setZoomData(instance, value){
+	window[instance].setValue(value);
 }
