14d685e7f8a7cb8d3339297dcb8ade58c5452cbf
Jucelio Horacio Vieira <jucelio.vieira@fluig.com>
2017-08-18 12:28:57 -0300

    Inserida função removedZoomItem

--- forms/smart-zoom/custom.js
+++ forms/smart-zoom/custom.js
@@ -1,25 +1,37 @@
 function setSelectedZoomItem(selectedItem) {
-	
+
 	var NAME = "colleagueName";
 	var EMAIL = "colleagueMail";
 	var ID = "colleagueId";
 
 	var FIELD = selectedItem.inputId;
-	
-	if(selectedItem.inputId == NAME){		
+
+	if(selectedItem.inputId == NAME){
 		setZoomData("colleagueMail", selectedItem["Email"]);
 		setZoomData("colleagueId", selectedItem["ID"]);
-		
-	}else if(selectedItem.inputId == EMAIL){			
+
+	}else if(selectedItem.inputId == EMAIL){
 		setZoomData("colleagueName", selectedItem["Nome"]);
-		setZoomData("colleagueId", selectedItem["ID"]);		
-	
+		setZoomData("colleagueId", selectedItem["ID"]);
+
 	}else if(selectedItem.inputId == ID){
 		setZoomData("colleagueName", selectedItem["Nome"]);
-		setZoomData("colleagueMail", selectedItem["Email"]);				
+		setZoomData("colleagueMail", selectedItem["Email"]);
 	}
 }
 
 function setZoomData(instance, value){
 	window[instance].setValue(value);
+}
+
+function removedZoomItem(removedItem) {
+
+	var NAME = "colleagueName";
+	var EMAIL = "colleagueMail";
+	var ID = "colleagueId";
+
+	if (item.inputId == NAME) {
+		console.log("Retornando resultado removedZoomItem");
+		console.log(item);
+	}
 }
