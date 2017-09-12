0ad4b22678fb4515d220d35439986d2ac43eae7d
Luiz Henrique Konishi Taira <luiz.taira@fluig.com>
2017-09-12 10:18:54 -0300

    Initial Commit

---
+++ sample-component-widget/target/classes/view.ftl
@@ -0,0 +1,6 @@
+<div id="sampleWidget_${instanceId}" class="wcm-widget-class super-widget" data-params="SampleWidget.instance()">
+    <h1>Hello World!</h1>
+    
+    <button type="button"  data-do-something>Click Me!</button>
+    
+</div>
---
+++ sample-component-widget/target/sample-component-widget/resources/js/samplewidget-min.js
@@ -0,0 +1,1 @@
+var SampleWidget=SuperWidget.extend({init:function(){},bindings:{local:{"do-something":["click_someFunc"]}},someFunc:function(){var d=new Date();var curr_date=d.getDate();var curr_month=d.getMonth()+1;var curr_year=d.getFullYear();alert("Hoje é : "+curr_date+"/"+curr_month+"/"+curr_year)}});
