1ba53e98daa86a3aaa04827cc5cdbc0fbefce731
Luiz Henrique Konishi Taira <luiz.taira@fluig.com>
2017-09-21 14:58:36 -0300

    Inserindo arquivos de traducao | alterando pom.xml para dependencias do fluig

--- sample-component-widget/src/main/webapp/resources/js/samplewidget.js
+++ sample-component-widget/src/main/webapp/resources/js/samplewidget.js
@@ -1,20 +1,25 @@
 var SampleWidget = SuperWidget.extend({
 	
 	init: function() {
-		//code
+		// se desejar, coloque aqui instruções iniciais de cada widget
 	},
 
 	bindings: {
+		// binding dos botões relacionados com cada função 
 		local: {
 			'do-something': ['click_someFunc']
 		}
 	},
 
-	someFunc: function() {
-		var d = new Date();		
+	/**
+	 * Função para exibir a data atual
+	 * @returns
+	 */
+	someFunc: function() {	
+		var d = new Date();
 	    var curr_date = d.getDate();
 	    var curr_month = d.getMonth() + 1;
 	    var curr_year = d.getFullYear();
-		alert("Hoje é : " + curr_date + "/" + curr_month + "/" + curr_year);
+		alert('${i18n.getTranslation("msg.today.is")}' +' :' + curr_date + '/' + curr_month + '/' + curr_year);
 	}
 })
