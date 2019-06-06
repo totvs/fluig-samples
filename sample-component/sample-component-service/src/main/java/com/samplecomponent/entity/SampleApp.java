165f642cad53cf286c12919328af92589525b4c0
Luiz Henrique K Taira <luizcomz@gmail.com>
2019-06-06 11:55:13 -0300

    correcoes de pom

--- sample-component/sample-component-service/src/main/java/com/samplecomponent/entity/SampleApp.java
+++ sample-component/sample-component-service/src/main/java/com/samplecomponent/entity/SampleApp.java
@@ -70,7 +70,6 @@ public class SampleApp implements Serializable {
 
 	public SampleApp(Long id, String name, String developer,
 			SampleCategory category, Long tenantId) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.developer = developer;
@@ -116,6 +115,61 @@ public class SampleApp implements Serializable {
 
 	public void setTenantId(Long tenantId) {
 		this.tenantId = tenantId;
+	}
+
+	@Override
+	public String toString() {
+		return "SampleApp [id=" + id + ", name=" + name + ", developer=" + developer + ", category=" + category
+				+ ", tenantId=" + tenantId + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((category == null) ? 0 : category.hashCode());
+		result = prime * result + ((developer == null) ? 0 : developer.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((tenantId == null) ? 0 : tenantId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SampleApp other = (SampleApp) obj;
+		if (category == null) {
+			if (other.category != null)
+				return false;
+		} else if (!category.equals(other.category))
+			return false;
+		if (developer == null) {
+			if (other.developer != null)
+				return false;
+		} else if (!developer.equals(other.developer))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (tenantId == null) {
+			if (other.tenantId != null)
+				return false;
+		} else if (!tenantId.equals(other.tenantId))
+			return false;
+		return true;
 	}
 
 }
--- sample-component/sample-component-service/src/main/java/com/samplecomponent/entity/SampleCategory.java
+++ sample-component/sample-component-service/src/main/java/com/samplecomponent/entity/SampleCategory.java
@@ -47,7 +47,6 @@ public class SampleCategory implements Serializable {
 	}
 
 	public SampleCategory(Long id, String name, Long tenantId) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.tenantId = tenantId;
@@ -76,5 +75,49 @@ public class SampleCategory implements Serializable {
 	public void setTenantId(Long tenantId) {
 		this.tenantId = tenantId;
 	}
+
+	@Override
+	public String toString() {
+		return "SampleCategory [id=" + id + ", name=" + name + ", tenantId=" + tenantId + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((tenantId == null) ? 0 : tenantId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SampleCategory other = (SampleCategory) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (tenantId == null) {
+			if (other.tenantId != null)
+				return false;
+		} else if (!tenantId.equals(other.tenantId))
+			return false;
+		return true;
+	}
+	
+	
 
 }
